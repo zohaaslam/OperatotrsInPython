ce496a491ba2f78d9878bcb7397a54a632f20b01
Zoha Aslam <160209815+zohaaslam@users.noreply.github.com>
2025-03-08 06:03:08 +0500

    Add files via upload

---
+++ operatorss.py
@@ -0,0 +1,161 @@
+# OPERATORS IN PYTHON
+# ARITHMETIC OPERATORS
+# ASSIGNMENT OPERATORS 
+# COMPARISSION OPERATORS
+# LOGICAL OPERATORS 
+# IDENTITY OPERATORS
+# MEMBERSHIP OPERATORS
+# BITWISE OPERATORS
+
+
+# ARITHMETIC OPERATORS
+
+# +
+a = 7
+b = 7
+print(a + b)
+
+# -
+c = 6
+d = 2
+print(c - d)
+
+#  *
+e = 8
+f = 3
+print(e * f)
+
+# / y hmesha decimal value m result deta h 
+g = 4
+h = 2
+print( g / h)
+
+#  // y hmesha integer value m result deta h 
+i = 8
+j = 2
+print(i // j)
+  
+# Exponential operators **
+
+k = 6 
+l = 2
+print(k ** l)
+
+# Modulus %
+m = 6
+n = 4
+print(m % n)
+
+
+# ASSIGNMENT OPERATORS
+# +=
+b = 4
+b += 6
+print(b)
+
+# -=
+c = 10
+c -=3
+print(c)
+
+# -/
+
+d = 44
+d /= 4
+print(d)
+
+# %=
+e = 20
+e %= 3
+print(e)
+
+# COMPARISION OPERATORS
+a = 6
+print(a == 7)
+
+#  !=
+i = 8
+print(i != 9)
+
+# less than equal to
+# <=
+c = 20
+print(c <= 10 )
+
+
+# greater than equal to
+f = 70
+print(f >= 90)
+
+
+
+# > greater than
+f = 40
+g = 90
+print(f > g)
+
+# < less than
+b = 10
+c = 11
+print(b < c)
+
+
+# LOGICAL OPERATORS
+# and y jb isme dono true hoge jb y true print krega 
+a = True
+b = False
+print(a and b)
+
+#  OR y jb isme koi ek bhi  true hoge jb y true print krega 
+b = True
+c = False
+print(b or c)
+
+# IDENTITY OPERATORS
+# IS / IS NOT
+
+# Is
+a = 3
+b = 3
+print(a is b)
+
+# Is not
+
+c = 5
+f = 5
+print(c is not f)
+
+
+# MEMBERSHIP OPERATOR
+# IN NOT IN
+
+# in
+list:int= [2,3,4,5,6,7,8,9]
+print(9 in list)
+
+
+# not in
+list:int= [2,3,90,5,6,7,8,9]
+print(900 not in list)
+
+# BITWISE OPERATORS
+
+c = 5
+d = 3
+print(c & d)
+
+a = 4
+b = 2
+print(a & b)
+
+# Bitwise OR |
+
+a = 9 
+b = 3
+print(a | b)
+
+
+e = 10
+f = 6
+print(e | f)
+
